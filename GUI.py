8b0f0cfae7c35ce191ef8b581f726272716d7055
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-10-12 17:07:49 -0400

    got some framework

--- GUI.py
+++ GUI.py
@@ -1,11 +1,18 @@
 from tkinter import * 
 import os
+import pandas as pd
+import openpyxl
+from openpyxl import Workbook
 
 class GUI:
     def __init__(self, master, size = "925x510"):
         self.master = master
         self.master.title("Sign In")
         self.master.geometry(size) #GUI start size
+
+        #set up workbook
+        self.wb = openpyxl.load_workbook('data.xlsx')
+        self.ws = self.wb.active
 
         #input box
         self.inputbox = Entry(self.master, font = "Helvetica 44 bold", width=25)
@@ -19,5 +26,10 @@ class GUI:
 
     #signin
     def signin(self):
-        identity = self.inputbox.get()
+        self.person = self.inputbox.get()
         self.inputbox.delete(0, 'end')
+
+        #excel stuff
+        self.name_col = 2
+        self.name_row = self.ws.cell(row=1,column=1).value
+        print(self.name_row)
