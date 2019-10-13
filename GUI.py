70276e61fa61f0fa902889eb8a4617f4a2df37a3
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-10-13 00:47:42 -0400

    adds new col for a new date

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 from tkinter import * 
 import os
-import pandas as pd
 import openpyxl
 from openpyxl import Workbook
+from datetime import date
 
 class GUI:
     def __init__(self, master, size = "925x510"):
@@ -15,6 +15,10 @@ class GUI:
         #set up workbook
         self.wb = openpyxl.load_workbook('data.xlsx')
         self.ws = self.wb.active
+
+        #label
+        self.label = Label(master, text="Enter Full Name", font="Helvetica 36 bold")
+        self.label.pack()
 
         #input box
         self.inputbox = Entry(self.master, font = "Helvetica 44 bold", width=25)
@@ -32,11 +36,19 @@ class GUI:
         self.inputbox.delete(0, 'end')
         #excel stuff
         self.name_col = 2
-        self.name_row = self.ws.cell(row=1,column=1).value
-        print(self.name_row)
+        self.name_row = self.ws.max_row+1
 
         #get names into array
         for row in range(2,self.ws.max_row+1):    #Here you can add or reduce the columns
             self.names.append(self.ws.cell(row=row,column=self.name_col).value) # the value of the specific cell
 
-        print(self.names)
+
+        #check for if today's column exists, if no make it
+        self.currentcol=self.ws.cell(row = 1, column=self.ws.max_column).value
+
+        if (self.currentcol==date.today()):
+            self.signin_col=self.ws.max_column
+        else:
+            self.ws.cell(row=1,column=(self.ws.max_column+1)).value = date.today()
+            self.signin_col=self.ws.max_column
+            self.wb.save('data.xlsx')
