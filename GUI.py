5b343401a49d60b0dd978346c79131c8b63796e6
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-10-12 23:51:50 -0400

    Got it to read all the names!

--- GUI.py
+++ GUI.py
@@ -10,6 +10,8 @@ class GUI:
         self.master.title("Sign In")
         self.master.geometry(size) #GUI start size
 
+        #set vars
+        self.names = []
         #set up workbook
         self.wb = openpyxl.load_workbook('data.xlsx')
         self.ws = self.wb.active
@@ -28,16 +30,13 @@ class GUI:
     def signin(self):
         self.person = self.inputbox.get()
         self.inputbox.delete(0, 'end')
-        self.names
         #excel stuff
         self.name_col = 2
         self.name_row = self.ws.cell(row=1,column=1).value
         print(self.name_row)
 
         #get names into array
-        for row in range(2,self.ws.max_row+1):  
-            for column in "B":  #Here you can add or reduce the columns
-                cell_name = "{}{}".format(column, row)
-                self.names.append(self.ws[cell_name].value) # the value of the specific cell
+        for row in range(2,self.ws.max_row+1):    #Here you can add or reduce the columns
+            self.names.append(self.ws.cell(row=row,column=self.name_col).value) # the value of the specific cell
 
         print(self.names)
