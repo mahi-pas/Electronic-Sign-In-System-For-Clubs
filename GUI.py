193e728bbf68cdbf44820a7aab8b7b6ae978a48e
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-10-13 14:52:46 -0400

    Got it to mark present!

--- GUI.py
+++ GUI.py
@@ -10,8 +10,6 @@ class GUI:
         self.master.title("Sign In")
         self.master.geometry(size) #GUI start size
 
-        #set vars
-        self.names = []
         #set up workbook
         self.wb = openpyxl.load_workbook('data.xlsx')
         self.ws = self.wb.active
@@ -34,21 +32,26 @@ class GUI:
     def signin(self):
         self.person = self.inputbox.get()
         self.inputbox.delete(0, 'end')
-        #excel stuff
+        #setting macros i guess(just makes it easier)
         self.name_col = 2
         self.name_row = self.ws.max_row+1
 
-        #get names into array
-        for row in range(2,self.ws.max_row+1):    #Here you can add or reduce the columns
-            self.names.append(self.ws.cell(row=row,column=self.name_col).value) # the value of the specific cell
-
-
         #check for if today's column exists, if no make it
         self.currentcol=self.ws.cell(row = 1, column=self.ws.max_column).value
-
-        if (self.currentcol==date.today()):
+        self.today = str(date.today())
+        if (self.currentcol==self.today):
             self.signin_col=self.ws.max_column
         else:
-            self.ws.cell(row=1,column=(self.ws.max_column+1)).value = date.today()
+            self.ws.cell(row=1,column=(self.ws.max_column+1)).value = self.today
             self.signin_col=self.ws.max_column
             self.wb.save('data.xlsx')
+
+        #finding name in list and checking them off
+        for row in range(2,self.ws.max_row+1):   
+            if (self.person.capitalize() == (self.ws.cell(row=row,column=self.name_col).value).capitalize()): #checks if the same
+                self.ws.cell(row=row,column=self.signin_col).value = "Present"
+                self.wb.save('data.xlsx')
+
+
+
+
